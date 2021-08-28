d271698992de51540d18760bc6ce0ca54c16b7ac
hanchchch <cndghks15@gmail.com>
2021-08-28 22:34:34 +0900

    fix: seed product descriptions

--- product/management/commands/seed.py
+++ product/management/commands/seed.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="T-Shirt Black",
-            description="Black T-Shirt",
+            description="심플한 무드의 KU 로고 프린팅 티셔츠\n크림슨 색배합과 캠퍼스 위치가 새겨져있어요.",
             price=16900,
         )
         prod.options.set([shirts_size])
@@ -32,7 +32,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="T-Shirt White",
-            description="White T-Shirt",
+            description="키치한 무드의 KLUB 오리지널 캐릭터 오버핏 티셔츠\n-학교 밖에서도 편하게 입을 수 있어요.",
             price=16900,
         )
         prod.options.set([shirts_size])
@@ -41,15 +41,15 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="Tumbler",
-            description="Tumbler",
+            description="카페, 학교 등에서 가볍게 쓰기 좋은 컴팩트한 사이즈에요.\n분리형 이중 구조로 되어있어 세척, 보온이 용이합니다.",
             price=7900,
         )
         prod.options.set([tumbler_color])
         prod.save()
 
         prod = ListProduct.objects.create(
-            name="Sticker",
-            description="Sticker",
+            name="Laptop Stickers",
+            description="무광 스티커 9종으로 구성된 KLUB의 오리지널 스티커 세트\n노트북, 핸드폰, 노트, 아이패드 등에\n붙이기 적당한 사이즈에요.\nKU의 무드로 주변을 꾸며보세요!",
             price=5900,
         )
         prod.fixed_options.set([sticker_fixed])
@@ -57,7 +57,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="Libertas Set",
-            description="Libertas Set",
+            description="티셔츠 & 스티커\n\n일상에서 소소하게 느낌내기 좋은 세트!",
             original_price=22800,
             price=20800,
         )
@@ -67,7 +67,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="Justitia Set",
-            description="Justitia Set",
+            description="티셔츠 & 텀블러\n\n이번 학기 과탑 예약! 공부할 때 쓰기 좋은 실용성 최고 세트",
             original_price=24800,
             price=22800,
         )
@@ -76,7 +76,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="Veritas Set",
-            description="Justitia Set",
+            description="스티커 & 텀플러\n\n수험생, 선후배 선물로 제격인 아이템 세트!",
             original_price=13800,
             price=11800,
         )
@@ -86,7 +86,7 @@ class Command(BaseCommand):
 
         prod = ListProduct.objects.create(
             name="Young-Tiger Set",
-            description="Young-Tiger Set",
+            description="티셔츠 & 스티커 & 텀블러\n\n모든 걸 다 담았다! 한정 판매 아이템들을 놓치기 싫은 당신을 위한 세트",
             original_price=30700,
             price=27700,
         )
